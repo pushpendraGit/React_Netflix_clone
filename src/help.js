8eddd1dfe7d5e45f097433b974e817c3c89915c0
Pushpendra Singh <pushpendra7060@gmail.com>
2020-07-23 09:58:29 +0530

    basic setip is created0

---
+++ src/help.js
@@ -0,0 +1,6 @@
+// API Key (v3 auth)
+// fedc7a7b6516f65c1b6f2b95de31fc8c
+// Example API Request
+// https://api.themoviedb.org/3/movie/550?api_key=fedc7a7b6516f65c1b6f2b95de31fc8c
+// API Read Access Token (v4 auth)
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmZWRjN2E3YjY1MTZmNjVjMWI2ZjJiOTVkZTMxZmM4YyIsInN1YiI6IjVmMTkwYTUxNTFlNmFiMDAzNmNkMmQ0MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.f8JHr4lZz98Ts4iBG0XWiw4aCDdU-HT1gXkysMLgI-Y
