69ffd23f4ec8bf1f62e3558916aa1840936c5c0e
Pushpendra Singh <pushpendra7060@gmail.com>
2020-07-26 00:41:21 +0530

    netfilx-app is created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React from "react";
 
-import './App.css';
-import Row from './Row';
-import requests from './requests';
-
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
+import Banner from "./Banner";
+import Nav from "./Nav";
 
 function App() {
   return (
-    <div className="App">
-      <h1>App</h1>
-      <Row 
-
-      isLargeRow = {true}
-      title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginls}/>
-      <Row title='Trending Now' fetchUrl={requests.fetchTrending} />
+    <div className="app">
+      <Nav />
+      <Banner />
+      <Row
+        isLargeRow={true}
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginls}
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
-      <Row title='Action Movies' fetchUrl={requests.fetchActonMovies} />
-      <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} />
-      <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} />
-      <Row title='Rommance Movies' fetchUrl={requests.fetchRomanceMovies} />
-      <Row title='Documentries' fetchUrl={requests.fetchDocumentarMovies} />
- 
+      <Row title="Action Movies" fetchUrl={requests.fetchActonMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Rommance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentries" fetchUrl={requests.fetchDocumentarMovies} />
     </div>
   );
 }
---
+++ src/Nav.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from "react";
+import './Nav.css'
+
+function Nav(props) {
+
+    const [show, handleShow] = useState(false)
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+
+            if(window.scrollY > 100){
+
+                handleShow(true)
+
+
+            }else  handleShow(false);
+
+          return () => {  
+              window.removeEventListener("scroll")
+            }
+
+        })
+
+    }, [])
+  return (
+    <div className={`nav ${show && "nav__black"}`}>
+      <img
+        className="nav__logo"
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/1920px-Netflix_2015_logo.svg.png"
+        alt="Netflix Logo"
+      />
+
+      <img
+        className="nav__avatar"
+        src="https://i.pinimg.com/originals/b6/77/cd/b677cd1cde292f261166533d6fe75872.png"
+        alt="Netflix Logo"
+      />
+    </div>
+  );
+}
+
+export default Nav;
